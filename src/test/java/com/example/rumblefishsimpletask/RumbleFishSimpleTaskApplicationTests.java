b7b68f53143d5eaf51838c9c5ebe13114406ffa5
Plywacz <m.plywacz@interia.pl>
2022-07-23 17:38:30 +0200

    init commit

---
+++ src/test/java/com/example/rumblefishsimpletask/RumbleFishSimpleTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rumblefishsimpletask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumbleFishSimpleTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
